d81b51b34893eefa4dfb5aa28f4d73a84ef3d84c
EduardoWisch <127266926+EduardoWisch@users.noreply.github.com>
2023-07-25 11:40:06 -0300

    Add files via upload

---
+++ 8-loops.js
@@ -0,0 +1,26 @@
+console.log(`\n Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Rio de Janeiro";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true; 
+
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{
+    console.log("Você não pode embarcar");
+}
+
+console.log(listaDeDestinos);
