fafeaca30a35b7d53d270e3b9a3f807e5693c6b6
EduardoWisch <127266926+EduardoWisch@users.noreply.github.com>
2023-07-31 21:16:04 -0300

    Add files via upload

--- 8-loops.js
+++ 8-loops.js
@@ -17,6 +17,7 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
 let destinoExiste = false;
+
 while (contador < 3) {
     if (listaDeDestinos[contador] == destino) {
         destinoExiste = true;
@@ -26,3 +27,16 @@ while (contador < 3) {
 } 
 
 console.log(`Destino existe: ${destinoExiste}`);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem!");
+}else{
+    console.log("Desculpe, tivemos um erro!")
+}
+
+for(let i = 0; i < 3; i ++) {
+    if (listaDeDestinos[i] == destino) {
+        destinoExiste = true;
+        break;
+    }
+} 
